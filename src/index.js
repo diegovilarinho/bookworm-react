e675e2d67834afd3ef34d684c84c3e36716ca1c6
diegovilarinho <contato@diegovilarinho.com.br>
2018-01-22 23:16:39 -0200

    Redux setup

--- src/index.js
+++ src/index.js
@@ -2,12 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import 'semantic-ui-css/semantic.min.css';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import rootReducer from './rootReducer';
+
+
+/*
+ * store = Store da minha aplicação
+ * rootReducer = Reducer que irá conter toda a árvore de estados, todos os meu objetos da store.
+ */
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </BrowserRouter>,
   document.getElementById('root')
 );
